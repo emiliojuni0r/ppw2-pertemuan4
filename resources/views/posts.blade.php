bfac8bf54cd733c504d5a851955f63db53050cbd
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-09-05 12:42:21 +0700

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Posts</h1>
+    <h2>{{ $data }}</h2>
+    <h2>{{ $data1 }}</h2>
+</body>
+</html>
